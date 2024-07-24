00ecca35309ae9ca97bba3793e905e9c2317c00d
WilliamBuntu <williambuntu1@gmail.com>
2024-07-25 01:28:11 +0200

    added card

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../widgets/meal_item.dart';
+
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
   final String title;
@@ -11,18 +13,19 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(meals[index].title),
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
     );
     if (meals.isEmpty) {
       content = Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-             Text('No meals found, please check your filters',
-            
-            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+            Text(
+              'No meals found, please check your filters',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onSurface,
-                  ),),
+                  ),
+            ),
             const SizedBox(
               height: 20,
             ),
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,13 +1,74 @@
 import 'package:flutter/material.dart';
 import '../models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'meal_item_trait.dart';
+
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: InkWell(
-      onTap: (){},
-      child: Stack(),),);
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(9),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 4,
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                fit: BoxFit.cover,
+                image: NetworkImage(meal.imageUrl),
+                height: 250,
+                width: double.infinity),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                color: Colors.black54,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 3, horizontal: 40),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // Text(
+                    //   '${meal.duration} min',
+                    //   style: const TextStyle(fontSize: 20, color: Colors.white),
+                    // ),
+                    const SizedBox(
+                      height: 9,
+                    ),
+                    Row(
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule, label: '${meal.duration} min')
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
